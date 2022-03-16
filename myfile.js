9eb36b39de14e6970b70b6f6d0d107d47000947f
sourov-sarker8251 <souravsarker2015@gmail.com>
2022-03-16 16:15:36 +0600

    java script

--- myfile.js
+++ myfile.js
@@ -1,2 +1,18 @@
-document.write("hello world");
-alert("hello World");
+// document.write("hello world");
+// alert("hello World");
+
+var x;
+x = "sourov sarker"
+document.write(typeof x)
+document.write("<br/>")
+document.write(5 / 3)
+
+document.write(x)
+
+/*
+eight types of data types
+we cant use reserve keywords
+* hello java script
+*
+*
+*  */
