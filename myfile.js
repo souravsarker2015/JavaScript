ba32cd374402ffa5056f2c5295824b8b823d9396
sourov-sarker8251 <souravsarker2015@gmail.com>
2022-03-18 14:40:59 +0600

    updated

--- myfile.js
+++ myfile.js
@@ -1,6 +1,33 @@
+x = 3
+
+switch (x) {
+    case 1:
+        document.write("saturday")
+        break;
+
+    case 2:
+        document.write("sunday")
+        break;
+
+    default:
+        document.write("input valid day number")
+}
+
+
 // document.write("hello world");
 // alert("hello World");
+// a = 6;
+// b = 7;
+// if (a > b) {
+//     document.write("a is greater")
+// } else if (a < b) {
+//     document.write("b is grater")
+// } else {
+//     document.write("b is greater")
+// }
 
+
+/*
 var x;
 x = "sourov sarker"
 document.write(typeof x)
@@ -8,10 +35,20 @@ document.write("<br/>")
 document.write(5 / 3)
 
 document.write(x)
+/*
 
+ */
 /*
 eight types of data types
 we cant use reserve keywords
+
+
+
+1. pre increment post increment
+
+
+
+
 * hello java script
 *
 *
